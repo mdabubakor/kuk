cbb6361276deed3a3292984af12a06198f8084f1
abubakar460 <mdsimul5060@gmail.com>
2023-10-22 06:14:28 -0700

    ha

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-center  bg-white text-black'>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Header
