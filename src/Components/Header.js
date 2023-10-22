a14d61f2f9de76fd8a1a22f04667a8b819677454
abubakar460 <mdsimul5060@gmail.com>
2023-10-22 06:19:28 -0700

    hoiche

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center  bg-white text-black'>
+    <div className='flex items-center justify-center bg-white  h-20 gap-5 text-black'>
       <p>Home</p>
       <p>Home</p>
       <p>Home</p>
